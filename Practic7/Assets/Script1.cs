d14bcb10789953fa514ce02d9320a66bfa564cb6
Leshka <31736716+TOPlessss@users.noreply.github.com>
2020-03-27 01:51:42 +0300

    practic7

---
+++ Practic7/Assets/Script1.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script1 : MonoBehaviour
+{
+   public float Speed;
+
+   void Update()
+   {
+      transform.Translate(transform.forward * Time.deltaTime * Speed);
+   }
+}
---
+++ Practic7/Assets/Script2.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script2 : MonoBehaviour
+{
+    public Vector3 Speed;
+    void Update()
+    {
+        GetComponent<Rigidbody>().velocity = Speed * Time.deltaTime;
+    }
+}
