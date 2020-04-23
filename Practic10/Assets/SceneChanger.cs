630bb56a464b9742d50f9bbea30565a767393a84
Leshka <31736716+TOPlessss@users.noreply.github.com>
2020-04-23 16:41:41 +0300

    practic10

---
+++ Practic10/Assets/SceneChanger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneChanger : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Application.LoadLevel("Scene1");
+        }
+        
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Application.LoadLevel("Scene2");
+        }
+    }
+}
