5824aa2741855ce1d3716e1fb33fa39e385125b6
Leshka <31736716+TOPlessss@users.noreply.github.com>
2020-04-23 15:42:04 +0300

    practic9

---
+++ Practic9/Assets/AudioPlay.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlay : MonoBehaviour
+{
+    public AudioSource SoundEff;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        SoundEff.Play();
+    }
+}
