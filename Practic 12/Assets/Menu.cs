032f84852a50f988118be87ede5dd25bfce5f50f
Leshka <31736716+TOPlessss@users.noreply.github.com>
2020-05-13 21:16:05 +0300

    Practic 12

---
+++ Practic 12/Assets/Menu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public void StartScene()
+    {
+        Application.LoadLevel("Scene2");
+    }
+
+    public void Options(GameObject window)
+    {
+        window.SetActive(true);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
