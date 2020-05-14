7d7224f5bdef0193e17f4b4abd2af0240bc3d3b3
Leshka <31736716+TOPlessss@users.noreply.github.com>
2020-05-14 20:39:13 +0300

    Laba3

---
+++ laba3/Assets/Navigation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Navigation : MonoBehaviour
+{
+   public void OpenC(GameObject gameObjectC)
+   {
+       gameObjectC.SetActive(true);
+   }
+
+   public void OpenE(GameObject gameObjectE)
+   {
+       gameObjectE.SetActive(true);
+   }
+}
