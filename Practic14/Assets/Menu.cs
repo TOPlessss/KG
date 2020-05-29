f6fe497c3ced4faa691f5f70b871e603ea3b7d6d
Leshka <31736716+TOPlessss@users.noreply.github.com>
2020-05-30 00:44:21 +0300

    Practic14

---
+++ Practic14/Assets/Menu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject BoxPrefab;
+    public GameObject SpherePrefab;
+    public GameObject PanelMenu;
+    public GameObject SpawnObject;
+    private bool isActive;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            if (isActive)
+            {
+                PanelMenu.SetActive(true);
+                isActive = true;
+            }
+            else
+            {
+                PanelMenu.SetActive(false);
+                isActive = false;
+            }
+        }
+    }
+
+    public void CreateBox()
+    {
+        Instantiate(BoxPrefab, SpawnObject.transform.position, SpawnObject.transform.rotation);
+    }
+    public void CreateSphere()
+    {
+        Instantiate(SpherePrefab, SpawnObject.transform.position, SpawnObject.transform.rotation);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
